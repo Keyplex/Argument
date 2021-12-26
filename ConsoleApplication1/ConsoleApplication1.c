d6ce2d79567fb1e86960a0d66c6ac43520fbc333
Pavel <51826654+1commando1@users.noreply.github.com>
2021-12-26 13:29:49 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main(int argc, char* argv[]) {
+
+    setlocale(LC_ALL, "Rus");
+    int i;
+    for (i = 0; i < argc; i++) {
+        printf("Аргумент %d: %s\n", i, argv[i]);
+    }
+    for (i = 0; i < argc; i++) {
+        printf("Аргумент %d: %s\n", i+1, argv[i]);
+    }
+    if (argc == 1) {
+        printf("Командная строка не имеет дополнительных аргументов\n");
+    }
+    return 0;
+}
